1695f168c5628f032ec737480865624849900986
Parth2684 <bhosle6006@gmail.com>
2026-08-24 21:12:23 +0530

    feat: add manage range for code for both 3 len and 4 len tuple

--- crates/lang-parse/src/lib.rs
+++ crates/lang-parse/src/lib.rs
@@ -1,1 +1,32 @@
 pub mod download;
+
+pub struct CodeRange {
+    start_line: u32,
+    start_column: u32,
+    end_line: u32,
+    end_column: u32,
+}
+
+pub enum FlexibleArray {
+    SizeThree([u32; 3]),
+    SizeFour([u32; 4]),
+}
+
+impl CodeRange {
+    pub fn from_array(range: FlexibleArray) -> Self {
+        match range {
+            FlexibleArray::SizeThree([start_line, start_column, end_column]) => Self {
+                start_line,
+                start_column,
+                end_line: start_line,
+                end_column,
+            },
+            FlexibleArray::SizeFour([start_line, start_column, end_line, end_column]) => Self {
+                start_line,
+                start_column,
+                end_line,
+                end_column,
+            },
+        }
+    }
+}
